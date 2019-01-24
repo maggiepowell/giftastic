85c3f15b14ed977ed1a6c31a5412f1b5e9e063e7
Maggie Powell <maggiepowell@Maggies-MacBook-Pro.local>
2019-01-24 13:38:20 -0800

    working gifs animate/still

--- assets/javascript.js
+++ assets/javascript.js
@@ -46,31 +46,27 @@ $("button").on("click", function() {
 
           $("#gifs-appear-here").append(gifDiv);
         }
+
+    //code for animated and static gifs
+    $('#gifs-appear-here img').on('click', function () {
+      var state = $(this).attr("data-state");
+      
+          if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+          console.log(state);
+
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+          console.log(state);
+        }
       });
-  });
-
-//code for animated and static gifs
-$("#gifs-appear-here").on("click", function() {
-  console.log(typeof $(this));
-    var state = $(this).attr("data-state");
-    console.log($(this)[0]);
-    console.log(state);
-    // If the clicked image's state is still, update its src attribute to its data-animate value. ****NOT WORKING*******
-    if (state === "still") {
-      $(this).attr("src", $(this).attr("data-animate"));
-      $(this).attr("data-state", "animate");
-      console.log(state);
-
-    } else {
-      $(this).attr("src", $(this).attr("data-still"));
-      $(this).attr("data-state", "still");
-      console.log(state);
-
-    }
+    });
   });
 
 
   });
 
-  //gif images loading and when clicked data-state updates from still to animate but gif doesn't play
-
+//need to take value from user input box and add to button array
+//make a function call that takes each topic in the array remakes the buttons on the page
